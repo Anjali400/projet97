ae90c7d232d99ec9b2e3a476edd6cb12984a03ce
Anjali Meena <72985811+Anjali400@users.noreply.github.com>
2022-06-16 12:48:07 +0100

    Update game.py

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ print('Number Guessing Game 🤔 ')
 number = random.randint(1,9)
  
 chances = 0 
-
+print("Guess a number (between 1 and 9):")
 while chances < 5:
     guess = int(input('Enter your guess  ↪ : ')) 
 
